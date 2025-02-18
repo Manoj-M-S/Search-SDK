13015005cd1643ead73351aeebfb4ec0fb30e85d
manoj <manoj.makarasu@gmail.com>
2025-02-18 16:38:39 +0530

    Removed Unwanted folders

--- dist/types/hooks/useSearch.d.ts
+++
@@ -1,8 +0,0 @@
-interface UseSearchReturn {
-    results: any[];
-    loading: boolean;
-    error: string | null;
-    search: (query: string) => void;
-}
-declare const useSearch: () => UseSearchReturn;
-export default useSearch;
